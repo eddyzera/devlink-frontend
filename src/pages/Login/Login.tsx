685509e51151c6db711610328ba591081897fe32
eddyzera <esilva644@gmail.com>
2023-07-23 14:19:03 -0300

    style: configuration style of tailwindcss

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Login: React.FunctionComponent = () => {
   return (
     <section>
-      <h1>login</h1>
+      <h1 className="text-dark-purple">login</h1>
     </section>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-purple': 'var(--purple-900)',
+        'middle-purple': 'var(--purple-200)',
+        'light-purple': 'var(--purple-100)',
+        'dark-gray': 'var(--gray-900)',
+        'middle-gray': 'var(--gray-800)',
+        'light-gray': 'var(--gray-500)',
+        gray: 'var(--gray-100)',
+        white: 'var(--white)',
+        'dark-red': 'var(--red-900)',
+      },
+      borderRadius: {
+        'radius-md': 'var(--radius-md)',
+      },
+      boxShadow: {
+        'shadow-md': 'var(--shadow-md)',
+      },
+      fontSize: {
+        'text-12': 'var(--text-sm)',
+        'text-16': 'var(--text-md)',
+        'text-32': 'var(--text-lg)',
+      },
+      lineHeight: {
+        'line-md': 'var(--line-height)',
+      },
+    },
   },
   plugins: [],
 }
